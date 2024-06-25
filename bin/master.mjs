c0ade2a5c3e10bb93ec5d0f4033429228a03c166
aiyuekuang <2092186738@qq.com>
2024-06-25 20:00:03 +0800

    init

--- bin/master.mjs
+++ bin/master.mjs
@@ -6,20 +6,11 @@ let execSyncFun = (cmd) => {
     return execSync(cmd, {cwd: process.cwd(), env: env}).toString();
 }
 
-let branch = "master";
 
 //获取当前分支，再试试
 let actBranch = execSyncFun("git branch").split('\n').find(line => line.startsWith('*')).split('*')[1].trim();
 execSyncFun(`git pull`)
-// execSyncFun(`git add -A`)
-// execSyncFun(`git commit -m "feat(BIN): 自动化发版"`)
-// console.log("自动化提交完成")
-// execSyncFun(`git push origin ${actBranch}`)
-execSyncFun(`git checkout ${branch}`)
-console.log(`切换到${branch}分支`)
-execSyncFun(`git merge --no-ff --no-commit origin ${actBranch}`)
+console.log(`拉取最新代码成功`)
 execSyncFun(`git commit -m "feat(BIN): 自动化发版"`)
-execSyncFun(`git push origin ${branch}`)
+execSyncFun(`git push origin ${actBranch}`)
 console.log("发版代码推送成功")
-execSyncFun(`git checkout ${actBranch}`)
-console.log(`测试发布完成，并切换回${actBranch}分支`)
