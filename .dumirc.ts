e44311aa813dbf226d26eb5b553bab93eb8ff73a
aiyuekuang <2092186738@qq.com>
2024-06-26 09:05:18 +0800

    init

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'dumi';
 
+
+const repo = 'durians'; // 项目名(也就是你的仓库名)
 export default defineConfig({
   outputPath: 'docs-dist',
   apiParser: {},
@@ -12,4 +14,6 @@ export default defineConfig({
       { type: 'component', dir: './src/components' },
     ],
   },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
