3aeae8fc17fda26828ee8feb821201700ef62d79
aiyuekuang <2092186738@qq.com>
2024-07-02 13:34:00 +0000

    Deploying to build from @ aiyuekuang/durians@70e63b054469719f8f683756a44b09f7861761bb 🚀

---
+++ meta__components.e1a96871.async.js
@@ -0,0 +1,190 @@
+"use strict";(self.webpackChunkdurians=self.webpackChunkdurians||[]).push([[838],{97389:function(f,r,n){n.r(r),n.d(r,{demos:function(){return P}});var _=n(15009),t=n.n(_),p=n(99289),c=n.n(p),o=n(67294),y=n(37738),P={"loginpro-demo-0":{component:o.memo(o.lazy(c()(t()().mark(function l(){var d,s,a,u;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.resolve().then(n.bind(n,51660));case 2:return d=e.sent,s=d.LoginPro,e.next=6,Promise.resolve().then(n.t.bind(n,67294,19));case 6:return a=e.sent,u=a.useEffect,e.abrupt("return",{default:function(){return o.createElement("div",{style:{width:"100%",overflow:"hidden"}},o.createElement(s,{loginFormFieldProps:{title:"\u5185\u90E8\u7BA1\u7406\u5E73\u53F0"}}))}});case 9:case"end":return e.stop()}},l)})))),asset:{type:"BLOCK",id:"loginpro-demo-0",refAtomIds:["LoginPro"],dependencies:{"index.jsx":{type:"FILE",value:`import {LoginPro} from "durians";
+import {useEffect} from "react";
+
+
+/** \u6309\u94AE\u7EC4 */
+export default () => {
+
+  return (
+    <div style={{
+      width: "100%",
+      overflow: "hidden"
+    }}>
+      <LoginPro
+        loginFormFieldProps={{
+          title: "\u5185\u90E8\u7BA1\u7406\u5E73\u53F0"
+        }}
+      />
+    </div>
+  );
+};`},durians:{type:"NPM",value:"0.0.30"},react:{type:"NPM",value:"18.3.1"}},entry:"index.jsx",title:"\u9ED8\u8BA4\u65E0\u624B\u673A\u9A8C\u8BC1\u7801\u767B\u5F55\u6A21\u5F0F"},context:{durians:n(51660),react:n(67294)},renderOpts:{compile:function(){var l=c()(t()().mark(function s(){var a,u=arguments;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},s)}));function d(){return l.apply(this,arguments)}return d}()}}}},90026:function(f,r,n){n.r(r),n.d(r,{demos:function(){return P}});var _=n(15009),t=n.n(_),p=n(99289),c=n.n(p),o=n(67294),y=n(42460),P={"modalpro-demo-0":{component:o.memo(o.lazy(c()(t()().mark(function l(){var d,s,a,u,h,e;return t()().wrap(function(i){for(;;)switch(i.prev=i.next){case 0:return i.next=2,Promise.resolve().then(n.bind(n,51660));case 2:return d=i.sent,s=d.ModalPro,i.next=6,Promise.resolve().then(n.t.bind(n,67294,19));case 6:return a=i.sent,u=a.useEffect,i.next=10,Promise.resolve().then(n.bind(n,60482));case 10:return h=i.sent,e=h.Button,i.abrupt("return",{default:function(){return o.createElement("div",{style:{width:"100%",overflow:"hidden"}},o.createElement(s,{Content:function(v){return o.createElement("div",null,"\u6211\u662F\u5185\u5BB9")}},o.createElement(e,{type:"primary"},"\u70B9\u51FA\u51FA\u5F39\u7A97")))}});case 13:case"end":return i.stop()}},l)})))),asset:{type:"BLOCK",id:"modalpro-demo-0",refAtomIds:["ModalPro"],dependencies:{"index.jsx":{type:"FILE",value:`import {ModalPro} from "durians";
+import {useEffect} from "react";
+import {Button} from "antd"
+
+
+export default () => {
+
+  return (
+    <div style={{
+      width: "100%",
+      overflow: "hidden"
+    }}>
+      <ModalPro
+        Content={(props) => {
+          return (<div>\u6211\u662F\u5185\u5BB9</div>)
+        }}
+      >
+        <Button type="primary">\u70B9\u51FA\u51FA\u5F39\u7A97</Button>
+      </ModalPro>
+    </div>
+  );
+};`},durians:{type:"NPM",value:"0.0.30"},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.19.0"}},entry:"index.jsx",title:"\u57FA\u7840\u4F7F\u7528"},context:{durians:n(51660),react:n(67294),antd:n(60482)},renderOpts:{compile:function(){var l=c()(t()().mark(function s(){var a,u=arguments;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},s)}));function d(){return l.apply(this,arguments)}return d}()}}}},78893:function(f,r,n){n.r(r),n.d(r,{demos:function(){return P}});var _=n(15009),t=n.n(_),p=n(99289),c=n.n(p),o=n(67294),y=n(47117),P={"tablepro-demo-0":{component:o.memo(o.lazy(c()(t()().mark(function l(){var d,s,a,u;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.resolve().then(n.bind(n,51660));case 2:return d=e.sent,s=d.TablePro,e.next=6,Promise.resolve().then(n.t.bind(n,67294,19));case 6:return a=e.sent,u=a.useEffect,e.abrupt("return",{default:function(){for(var i=function(){console.log("\u53D6\u6D88")},O=function(){console.log("\u786E\u5B9A")},I=function(){console.log("\u70B9\u51FB")},v=[],m=0;m<50;m++)v.push({key:m,name:"Edward ".concat(m),age:m,address:"London Park no. ".concat(m),name1:"London Park no. ".concat(m,"1111"),name2:"London Park no. ".concat(m,"2222"),name3:"London Park no. ".concat(m,"2222")});return o.createElement("div",{style:{width:"100%",overflow:"hidden"}},o.createElement(s,{className:"xlb_table_demo",tableAlertOptionRenderPro:[function(E){return o.createElement("div",null,"111")}],bordered:!0,fieldProps:{rowKey:"key",dataSource:v,columns:[{title:"Full Name",dataIndex:"name",width:100,key:"name",sorter:!0,fixed:"left",align:"center"},{title:"Age",width:100,dataIndex:"age",key:"age",sorter:!0,children:[{title:"Name1",width:100,dataIndex:"name1",key:"name",sorter:!0,children:[{title:"Name3",width:100,dataIndex:"name3",key:"name",sorter:!0}]},{title:"Name2",width:100,dataIndex:"name2",key:"name"}]},{title:"Column 1",dataIndex:"address",key:"1",width:500}]}}))}});case 9:case"end":return e.stop()}},l)})))),asset:{type:"BLOCK",id:"tablepro-demo-0",refAtomIds:["TablePro"],dependencies:{"index.jsx":{type:"FILE",value:`import {TablePro} from "durians";
+import {useEffect} from "react";
+
+
+/** \u6309\u94AE\u7EC4 */
+export default () => {
+  const onCancel = () => {
+    console.log('\u53D6\u6D88');
+  };
+  const onOk = () => {
+    console.log('\u786E\u5B9A');
+  };
+  const onClick = () => {
+    console.log('\u70B9\u51FB');
+  };
+
+
+  const data = [];
+  for (let i = 0; i < 50; i++) {
+    data.push({
+      key: i,
+      name: \`Edward \${i}\`,
+      age: i,
+      address: \`London Park no. \${i}\`,
+      name1: \`London Park no. \${i}1111\`,
+      name2: \`London Park no. \${i}2222\`,
+      name3: \`London Park no. \${i}2222\`,
+    });
+  }
+
+
+  return (
+    <div style={{
+      width: "100%",
+      overflow: "hidden"
+    }}>
+
+      <TablePro
+        className={"xlb_table_demo"}
+        // rowHeight={200}
+        tableAlertOptionRenderPro={[(props)=>{
+          return <div>111</div>
+        }]}
+        bordered={true}
+        fieldProps={{
+          
+          rowKey:"key",
+          dataSource: data,
+          columns: [
+            {
+              title: 'Full Name',
+              dataIndex: 'name',
+              width: 100,
+              key: 'name',
+              sorter: true,
+              fixed: 'left',
+              align: "center"
+              // },{
+              //   title: 'Full Name222',
+              //   dataIndex: 'age',
+              //   align:"center",
+              //   width: 100,
+
+            },
+            {
+              title: 'Age',
+              width: 100,
+              dataIndex: 'age',
+              key: 'age',
+              sorter: true,
+
+              children: [
+                {
+                  title: 'Name1',
+                  width: 100,
+                  dataIndex: 'name1',
+                  key: 'name',
+                  sorter: true,
+                  children: [
+                    {
+                      title: 'Name3',
+                      width: 100,
+                      dataIndex: 'name3',
+                      key: 'name',
+                      sorter: true,
+                    }]
+                }, {
+                  title: 'Name2',
+                  width: 100,
+                  dataIndex: 'name2',
+                  key: 'name',
+                },
+              ]
+            },
+            {
+              title: 'Column 1',
+              dataIndex: 'address',
+              key: '1',
+              width: 500,
+            },
+            // {
+            //   title: 'Column 2',
+            //   dataIndex: '2',
+            //   key: '2',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 3',
+            //   dataIndex: '3',
+            //   key: '3',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 4',
+            //   dataIndex: '4',
+            //   key: '4',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 5',
+            //   dataIndex: '5',
+            //   key: '5',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 6',
+            //   dataIndex: '6',
+            //   key: '6',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 7',
+            //   dataIndex: '7',
+            //   key: '7',
+            //   width: 150,
+            // },
+            // {title: 'Column 8', dataIndex: '8', key: '8'},
+            // {
+            //   title: 'Action',
+            //   dataIndex: 'operation',
+            //   key: 'operation',
+            //   width: 100,
+            //   render: () => <a>action</a>,
+            // },
+          ]
+        }}
+      />
+    </div>
+  );
+};`},durians:{type:"NPM",value:"0.0.30"},react:{type:"NPM",value:"18.3.1"}},entry:"index.jsx",title:"\u57FA\u7840\u4F7F\u7528"},context:{durians:n(51660),react:n(67294)},renderOpts:{compile:function(){var l=c()(t()().mark(function s(){var a,u=arguments;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,u));case 3:case"end":return e.stop()}},s)}));function d(){return l.apply(this,arguments)}return d}()}}}},54394:function(f,r,n){n.r(r),n.d(r,{texts:function(){return t}});var _=n(37738);const t=[{value:"\u5BC6\u7801\u7684\u52A0\u5BC6\u4F7F\u7528\u7684\u662FCryptoJS.SHA256",paraId:0,tocIndex:0},{value:"\u53D1\u9001\u77ED\u4FE1\u9A8C\u8BC1\u7801\u7684\u5B57\u6BB5\u662Fmobile",paraId:1,tocIndex:0}]},23876:function(f,r,n){n.r(r),n.d(r,{texts:function(){return t}});var _=n(42460);const t=[]},53308:function(f,r,n){n.r(r),n.d(r,{texts:function(){return t}});var _=n(47117);const t=[]}}]);
