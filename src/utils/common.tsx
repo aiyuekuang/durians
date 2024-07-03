3eac8041d2caa52818e28345821a085fa228eaff
zengtao <2092186738@qq.com>
2024-07-03 09:15:39 +0800

    init

--- src/utils/common.tsx
+++ src/utils/common.tsx
@@ -19,13 +19,13 @@ export const ajaxCommon = (url: string, params: object, callback: Function, isAp
 }
 
 //通用处理Form的columns
-export const commonFormHandler = (columns:any[],ajax:any)=>{
+export const commonFormHandler = (columns:any,ajax:any)=>{
   for (let i of columns){
     let proConfig = i.proConfig
     if(i.valueType === "select"){
       i.request= async (params:any, props:any) => {
         let arr:any = []
-        await ajax(proConfig.url, {...(typeof proConfig.params === "function"?proConfig.params(params,props):proConfig.params)}, (data) => {
+        await ajax(proConfig.url, {...(typeof proConfig.params === "function"?proConfig.params(params,props):proConfig.params)}, (data:any) => {
           console.log(2222, data)
           arr = proConfig.setData(data)
         })
