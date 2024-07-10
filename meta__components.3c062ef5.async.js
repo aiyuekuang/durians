625149b0ca6ce925d419f8b39e7d919661b9d0f9
aiyuekuang <2092186738@qq.com>
2024-07-10 03:51:24 +0000

    Deploying to build from @ aiyuekuang/durians@d7ac1723cb8266e6a9a920d46671fa735e35d3c1 🚀

---
+++ meta__components.3c062ef5.async.js
@@ -0,0 +1,289 @@
+"use strict";(self.webpackChunkdurians=self.webpackChunkdurians||[]).push([[838],{32450:function(f,r,n){n.r(r),n.d(r,{demos:function(){return E}});var m=n(15009),t=n.n(m),h=n(99289),_=n.n(h),o=n(67294),y=n(34986),E={"formpro-demo-customform":{component:o.memo(o.lazy(function(){return n.e(567).then(n.bind(n,23882))})),asset:{type:"BLOCK",id:"formpro-demo-customform",refAtomIds:["FormPro"],dependencies:{"index.tsx":{type:"FILE",value:n(3110).Z},"@ant-design/pro-components":{type:"NPM",value:"2.7.10"},antd:{type:"NPM",value:"5.19.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@ant-design/pro-components":n(35961),antd:n(43886),react:n(67294)},renderOpts:{compile:function(){var u=_()(t()().mark(function s(){var a,l=arguments;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,l));case 3:case"end":return e.stop()}},s)}));function d(){return u.apply(this,arguments)}return d}()}}}},97389:function(f,r,n){n.r(r),n.d(r,{demos:function(){return E}});var m=n(15009),t=n.n(m),h=n(99289),_=n.n(h),o=n(67294),y=n(37738),E={"loginpro-demo-0":{component:o.memo(o.lazy(_()(t()().mark(function u(){var d,s,a,l;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,Promise.resolve().then(n.bind(n,87391));case 2:return d=e.sent,s=d.LoginPro,e.next=6,Promise.resolve().then(n.t.bind(n,67294,19));case 6:return a=e.sent,l=a.useEffect,e.abrupt("return",{default:function(){return o.createElement("div",{style:{width:"100%",overflow:"hidden"}},o.createElement(s,{loginFormFieldProps:{title:"\u5185\u90E8\u7BA1\u7406\u5E73\u53F0"}}))}});case 9:case"end":return e.stop()}},u)})))),asset:{type:"BLOCK",id:"loginpro-demo-0",refAtomIds:["LoginPro"],dependencies:{"index.jsx":{type:"FILE",value:`import {LoginPro} from "durians";
+import {useEffect} from "react";
+
+
+/** \u6309\u94AE\u7EC4 */
+export default () => {
+
+  return (
+    <div style={{
+      width: "100%",
+      overflow: "hidden"
+    }}>
+      <LoginPro
+        loginFormFieldProps={{
+          title: "\u5185\u90E8\u7BA1\u7406\u5E73\u53F0"
+        }}
+      />
+    </div>
+  );
+};`},durians:{type:"NPM",value:"0.0.43"},react:{type:"NPM",value:"18.3.1"}},entry:"index.jsx",title:"\u9ED8\u8BA4\u65E0\u624B\u673A\u9A8C\u8BC1\u7801\u767B\u5F55\u6A21\u5F0F"},context:{durians:n(87391),react:n(67294)},renderOpts:{compile:function(){var u=_()(t()().mark(function s(){var a,l=arguments;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,l));case 3:case"end":return e.stop()}},s)}));function d(){return u.apply(this,arguments)}return d}()}}}},90026:function(f,r,n){n.r(r),n.d(r,{demos:function(){return E}});var m=n(15009),t=n.n(m),h=n(99289),_=n.n(h),o=n(67294),y=n(42460),E={"modalpro-demo-0":{component:o.memo(o.lazy(_()(t()().mark(function u(){var d,s,a,l,p,e;return t()().wrap(function(i){for(;;)switch(i.prev=i.next){case 0:return i.next=2,Promise.resolve().then(n.bind(n,87391));case 2:return d=i.sent,s=d.ModalPro,i.next=6,Promise.resolve().then(n.t.bind(n,67294,19));case 6:return a=i.sent,l=a.useEffect,i.next=10,Promise.resolve().then(n.bind(n,43886));case 10:return p=i.sent,e=p.Button,i.abrupt("return",{default:function(){return o.createElement("div",{style:{width:"100%",overflow:"hidden"}},o.createElement(s,{Content:function(I){return o.createElement("div",null,"\u6211\u662F\u5185\u5BB9")}},o.createElement(e,{type:"primary"},"\u70B9\u51FA\u51FA\u5F39\u7A97")))}});case 13:case"end":return i.stop()}},u)})))),asset:{type:"BLOCK",id:"modalpro-demo-0",refAtomIds:["ModalPro"],dependencies:{"index.jsx":{type:"FILE",value:`import {ModalPro} from "durians";
+import {useEffect} from "react";
+import {Button} from "antd"
+
+
+export default () => {
+
+  return (
+    <div style={{
+      width: "100%",
+      overflow: "hidden"
+    }}>
+      <ModalPro
+        Content={(props) => {
+          return (<div>\u6211\u662F\u5185\u5BB9</div>)
+        }}
+      >
+        <Button type="primary">\u70B9\u51FA\u51FA\u5F39\u7A97</Button>
+      </ModalPro>
+    </div>
+  );
+};`},durians:{type:"NPM",value:"0.0.43"},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.19.1"}},entry:"index.jsx",title:"\u57FA\u7840\u4F7F\u7528"},context:{durians:n(87391),react:n(67294),antd:n(43886)},renderOpts:{compile:function(){var u=_()(t()().mark(function s(){var a,l=arguments;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,l));case 3:case"end":return e.stop()}},s)}));function d(){return u.apply(this,arguments)}return d}()}}}},78893:function(f,r,n){n.r(r),n.d(r,{demos:function(){return E}});var m=n(15009),t=n.n(m),h=n(99289),_=n.n(h),o=n(67294),y=n(47117),E={"tablepro-demo-0":{component:o.memo(o.lazy(_()(t()().mark(function u(){var d,s,a,l,p;return t()().wrap(function(c){for(;;)switch(c.prev=c.next){case 0:return c.next=2,Promise.resolve().then(n.bind(n,87391));case 2:return d=c.sent,s=d.TablePro,a=d.FormPro,c.next=7,Promise.resolve().then(n.t.bind(n,67294,19));case 7:return l=c.sent,p=l.useEffect,c.abrupt("return",{default:function(){for(var O=function(){console.log("\u53D6\u6D88")},D=function(){console.log("\u786E\u5B9A")},I=function(){console.log("\u70B9\u51FB")},M=[],P=0;P<50;P++)M.push({key:P,name:"Edward ".concat(P),age:P,address:"London Park no. ".concat(P),name1:"London Park no. ".concat(P,"1111"),name2:"London Park no. ".concat(P,"2222"),name3:"London Park no. ".concat(P,"2222")});return o.createElement("div",{style:{width:"100%",overflow:"hidden"}},o.createElement(s,{className:"xlb_table_demo",tableAlertOptionRenderPro:[function(v){return o.createElement(a,null)}],bordered:!0,fieldProps:{rowKey:"key",dataSource:M,columns:[{title:"Full Name",dataIndex:"name",width:100,key:"name",sorter:!0,fixed:"left",valueType:"treeSelectPro",align:"center"},{title:"Age",width:100,dataIndex:"age",key:"age",sorter:!0,children:[{title:"Name1",width:100,dataIndex:"name1",key:"name",sorter:!0,children:[{title:"Name3",width:100,dataIndex:"name3",key:"name",sorter:!0}]},{title:"Name2",width:100,dataIndex:"name2",key:"name"}]},{title:"Column 1",dataIndex:"address",key:"1",width:500}]}}))}});case 10:case"end":return c.stop()}},u)})))),asset:{type:"BLOCK",id:"tablepro-demo-0",refAtomIds:["TablePro"],dependencies:{"index.jsx":{type:"FILE",value:`import {TablePro,FormPro} from "durians";
+import {useEffect} from "react";
+
+
+/** \u6309\u94AE\u7EC4 */
+export default () => {
+  const onCancel = () => {
+    console.log('\u53D6\u6D88');
+  };
+  const onOk = () => {
+    console.log('\u786E\u5B9A');
+  };
+  const onClick = () => {
+    console.log('\u70B9\u51FB');
+  };
+
+
+  const data = [];
+  for (let i = 0; i < 50; i++) {
+    data.push({
+      key: i,
+      name: \`Edward \${i}\`,
+      age: i,
+      address: \`London Park no. \${i}\`,
+      name1: \`London Park no. \${i}1111\`,
+      name2: \`London Park no. \${i}2222\`,
+      name3: \`London Park no. \${i}2222\`,
+    });
+  }
+
+
+  return (
+    <div style={{
+      width: "100%",
+      overflow: "hidden"
+    }}>
+
+      <TablePro
+        className={"xlb_table_demo"}
+        // rowHeight={200}
+        tableAlertOptionRenderPro={[(props)=>{
+          return <FormPro />
+        }]}
+        bordered={true}
+        fieldProps={{
+          
+          rowKey:"key",
+          dataSource: data,
+          columns: [
+            {
+              title: 'Full Name',
+              dataIndex: 'name',
+              width: 100,
+              key: 'name',
+              sorter: true,
+              fixed: 'left',
+              valueType:"treeSelectPro",
+              align: "center"
+              // },{
+              //   title: 'Full Name222',
+              //   dataIndex: 'age',
+              //   align:"center",
+              //   width: 100,
+
+            },
+            {
+              title: 'Age',
+              width: 100,
+              dataIndex: 'age',
+              key: 'age',
+              sorter: true,
+
+              children: [
+                {
+                  title: 'Name1',
+                  width: 100,
+                  dataIndex: 'name1',
+                  key: 'name',
+                  sorter: true,
+                  children: [
+                    {
+                      title: 'Name3',
+                      width: 100,
+                      dataIndex: 'name3',
+                      key: 'name',
+                      sorter: true,
+                    }]
+                }, {
+                  title: 'Name2',
+                  width: 100,
+                  dataIndex: 'name2',
+                  key: 'name',
+                },
+              ]
+            },
+            {
+              title: 'Column 1',
+              dataIndex: 'address',
+              key: '1',
+              width: 500,
+            },
+            // {
+            //   title: 'Column 2',
+            //   dataIndex: '2',
+            //   key: '2',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 3',
+            //   dataIndex: '3',
+            //   key: '3',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 4',
+            //   dataIndex: '4',
+            //   key: '4',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 5',
+            //   dataIndex: '5',
+            //   key: '5',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 6',
+            //   dataIndex: '6',
+            //   key: '6',
+            //   width: 150,
+            // },
+            // {
+            //   title: 'Column 7',
+            //   dataIndex: '7',
+            //   key: '7',
+            //   width: 150,
+            // },
+            // {title: 'Column 8', dataIndex: '8', key: '8'},
+            // {
+            //   title: 'Action',
+            //   dataIndex: 'operation',
+            //   key: 'operation',
+            //   width: 100,
+            //   render: () => <a>action</a>,
+            // },
+          ]
+        }}
+      />
+    </div>
+  );
+};`},durians:{type:"NPM",value:"0.0.43"},react:{type:"NPM",value:"18.3.1"}},entry:"index.jsx",title:"\u57FA\u7840\u4F7F\u7528"},context:{durians:n(87391),react:n(67294)},renderOpts:{compile:function(){var u=_()(t()().mark(function s(){var a,l=arguments;return t()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(335).then(n.bind(n,37335));case 2:return e.abrupt("return",(a=e.sent).default.apply(a,l));case 3:case"end":return e.stop()}},s)}));function d(){return u.apply(this,arguments)}return d}()}}}},45637:function(f,r,n){n.r(r),n.d(r,{texts:function(){return t}});var m=n(34986);const t=[]},54394:function(f,r,n){n.r(r),n.d(r,{texts:function(){return t}});var m=n(37738);const t=[{value:"\u5BC6\u7801\u7684\u52A0\u5BC6\u4F7F\u7528\u7684\u662FCryptoJS.SHA256",paraId:0,tocIndex:0},{value:"\u53D1\u9001\u77ED\u4FE1\u9A8C\u8BC1\u7801\u7684\u5B57\u6BB5\u662Fmobile",paraId:1,tocIndex:0}]},23876:function(f,r,n){n.r(r),n.d(r,{texts:function(){return t}});var m=n(42460);const t=[]},53308:function(f,r,n){n.r(r),n.d(r,{texts:function(){return t}});var m=n(47117);const t=[]},3110:function(f,r){r.Z=`import {
+  ProForm,
+  ProFormDependency,
+  ProFormList,
+  ProFormText,
+} from '@ant-design/pro-components';
+import { Button } from 'antd';
+import React from 'react';
+
+const Demo = () => {
+  return (
+    <ProForm
+      onFinish={(values)=>{
+        console.log(55566,values)
+      }}
+    >
+      <ProFormList
+        name={['default', 'users']}
+        label="\u7528\u6237\u4FE1\u606F"
+        initialValue={[
+          {
+            name: '1111',
+          },
+        ]}
+        itemContainerRender={(doms) => {
+          return <ProForm.Group>{doms}</ProForm.Group>;
+        }}
+        alwaysShowItemLabel
+      >
+        {(f, index, action) => {
+          console.log(f, index, action);
+          return (
+            <>
+              <ProFormText
+                initialValue={index}
+                name="rowKey"
+                label={\`\u7B2C \${index} \u914D\u7F6E\`}
+              />
+              <ProFormText name={"name" + index} key="name" label="\u59D3\u540D" />
+              <ProFormDependency key="remark" name={['name']}>
+                {({ name }) => {
+                  if (!name) {
+                    return (
+                      <span
+                        style={{
+                          lineHeight: '92px',
+                        }}
+                      >
+                        \u8F93\u5165\u59D3\u540D\u5C55\u793A
+                      </span>
+                    );
+                  }
+                  return <ProFormText name={"remark" + index} label="\u6635\u79F0\u8BE6\u60C5" />;
+                }}
+              </ProFormDependency>
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'flex-end',
+                  gap: '8px',
+                  height: 60,
+                }}
+              >
+                <Button
+                  type="primary"
+                  key="SET"
+                  onClick={() => {
+                    action.setCurrentRowData({
+                      name: 'New Name' + index,
+                      remark: 'New Remark' + index,
+                    });
+                  }}
+                >
+                  \u8BBE\u7F6E\u6B64\u9879
+                </Button>
+
+                <Button
+                  type="dashed"
+                  key="clear"
+                  onClick={() => {
+                    action.setCurrentRowData({
+                      name: undefined,
+                      remark: undefined,
+                    });
+                  }}
+                >
+                  \u6E05\u7A7A\u6B64\u9879
+                </Button>
+              </div>
+            </>
+          );
+        }}
+      </ProFormList>
+    </ProForm>
+  );
+};
+
+export default Demo;
+`}}]);
