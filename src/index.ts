8cbc0adae4344d422bd7fb9ae726e8aab4317af1
zengtao <2092186738@qq.com>
2024-12-23 11:45:54 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export { default as TablePro } from './components/TablePro';
 export { default as ModalPro } from './components/ModalPro';
 export { default as LoginPro } from './components/LoginPro';
 export { default as FormPro } from './components/FormPro';
-export { default as TreePro } from './components/FormItem/TreePro';
+export { default as TreePro } from './components/TreePro';
